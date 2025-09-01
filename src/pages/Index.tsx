953dfd4f1e11fe4bb417a85858413709ac2f0a4d
Hao Tian <h49tian@uwaterloo.ca>
2025-08-31 21:16:39 -0400

    give up

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,33 @@
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import CollectionSection from "@/components/CollectionSection";
-import AboutSection from "@/components/AboutSection";
-import ServicesSection from "@/components/ServicesSection";
-import Footer from "@/components/Footer";
-import { Link } from "react-router-dom";
-const Index = () => {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      <HeroSection />
-      <CollectionSection />
-      <AboutSection />
-      <ServicesSection />
-      <Footer />
-    </div>
-  );
-};
+// import Header from "@/components/Header";
+// import HeroSection from "@/components/HeroSection";
+// import CollectionSection from "@/components/CollectionSection";
+// import AboutSection from "@/components/AboutSection";
+// import ServicesSection from "@/components/ServicesSection";
+// import Footer from "@/components/Footer";
+// import { Link } from "react-router-dom";
+// const Index = () => {
+//   return (
+//     <div className="min-h-screen">
+//       <Header />
+//       <HeroSection />
+//       <CollectionSection />
+//       <AboutSection />
+//       <ServicesSection />
+//       <Footer />
+//     </div>
+//   );
+// };
 
+// export default Index;
+console.log('Index route matched'); 
+function Index() {
+  return (
+    
+    <div style={{ backgroundColor: 'red', height: '100vh', color: 'white', fontSize: '50px' }}>
+      Test Content - This should be visible!
+    </div>
+    
+  );
+  
+}
 export default Index;
